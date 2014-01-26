07ab1c010fe8309e807a12a3989f3219b52827f3
Jonathan Xu <jonathan@randomguava.com>
2014-01-25 22:48:50 -0800

    add edit/done button

--- CP.ToDo/CPToDoListViewController.m
+++ CP.ToDo/CPToDoListViewController.m
@@ -57,6 +57,16 @@
     [toDoCell.toDoTextField becomeFirstResponder];
 }
 
+- (IBAction)touchEdit:(id)sender {
+    UIBarButtonItem *button = (UIBarButtonItem *)sender;
+    if ([button.title isEqualToString:@"Edit"]) {
+        button.title = @"Done";
+        [self.tableView setEditing:YES animated:YES];
+    } else {
+        button.title = @"Edit";
+        [self.tableView setEditing:NO animated:YES];
+    }
+}
 
 #pragma mark - Table view data source
 
@@ -81,17 +91,12 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
-*/
 
-/*
-// Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
@@ -102,23 +107,21 @@
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
-*/
 
-/*
-// Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
 {
+    NSLog(@"CPToDoListViewController.moveRowAtIndexPath: from %d to %d",
+          fromIndexPath.row, toIndexPath.row);
+    
+    NSString *temp = self.toDoList[toIndexPath.row];
+    self.toDoList[toIndexPath.row] = self.toDoList[fromIndexPath.row];
+    self.toDoList[fromIndexPath.row] = temp;
 }
-*/
 
-/*
-// Override to support conditional rearranging of the table view.
 - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Return NO if you do not want the item to be re-orderable.
     return YES;
 }
-*/
 
 #pragma mark - UITextFieldDelegate
 
--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Implementation Notes:
 - Use a custom UITableViewCell backed by CPToDoCell
   - it has a UITextField which user can input text values for their TODOs
 - A "+" Bar Button Item is used to add a new TODO cell at top of list, and automatically becomes the first responder and brings up keyboard
-- CPToDoCell's UITextField's delegate is set up CPToDoListViewController
+- CPToDoCell's UITextField's delegate is set to CPToDoListViewController
   - CPToDoListViewController uses UITextField's tag property to update its internal NSMutableArray which holds the TODO list
